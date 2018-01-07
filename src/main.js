dd3ad27afd297225abb029673827e7e6ad986c74
Ayush Goyal <ayush987goyal@gmail.com>
2018-01-07 23:13:24 +0530

    added toolbar and sidenav with nav items

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.css';
+import colors from 'vuetify/es5/util/colors';
+
+import App from './App';
+import router from './router';
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.red.darken2,
+    accent: colors.red.accent2,
+    secondary: colors.grey.lighten1,
+    info: colors.blue.lighten1,
+    warning: colors.amber.darken2,
+    error: colors.red.accent4,
+    success: colors.green.lighten2,
+  },
+});
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App),
+});
