dfd233db44f1f2c17c7e10758c4db3247e8fc8b9
Ayush Goyal <ayush987goyal@gmail.com>
2018-01-09 02:11:55 +0530

    added create meetup form component

--- src/store/index.js
+++ src/store/index.js
@@ -23,8 +23,25 @@ export const store = new Vuex.Store({
             registeredMeetups: ['ewtfgbeeergfvfdgef'],
         },
     },
-    mutations: {},
-    actions: {},
+    mutations: {
+        createMeetup(state, payload) {
+            state.loadedMeetups.push(payload);
+        },
+    },
+    actions: {
+        createMeetup({ commit }, payload) {
+            const meetup = {
+                title: payload.title,
+                location: payload.location,
+                imageUrl: payload.imageUrl,
+                description: payload.description,
+                date: payload.date,
+                id: 'sdafsdgevegv',
+            };
+            // Reach out to firebase and store it
+            commit('createMeetup', meetup);
+        },
+    },
     getters: {
         loadedMeetups(state) {
             return state.loadedMeetups.sort((meetupA, meetupB) => meetupA.date > meetupB.date);
